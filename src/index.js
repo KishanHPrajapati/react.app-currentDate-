68796f317dae0723b6798d8f26e0834b8c425bea
KishanHPrajapati <kishanprajapati8767@gmail.com>
2022-07-28 10:38:43 -0700

    currentDate program

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+let currDate= new Date(2022, 7, 22, 20);
+currDate = currDate.getHours();
+let img = "/logo192.png";
+let greeting = "";
+ const cssStyle = {
+  
+};
+
+if (currDate >= 1 && currDate < 12){
+  greeting = "good morning";
+  cssStyle.color =" blue";
+  
+}else if (currDate >= 12 && currDate < 19){
+  greeting = "good afternoon";
+  cssStyle.color = "yellow";
+
+}
+else {
+  greeting = "good night";
+  cssStyle.color = 'orange';
+}
+ ReactDOM.render(
+
+ <>
+ <div>
+
+ <span className='kp'><img src='/logo192.png'  alt='blank'/></span> 
+ <h1 className='k'>Hello sir, <span style={cssStyle}> {greeting } </span>
+ </h1>
+
+  <span className='ks'>
+ <img src='/logo192.png'  alt='blank'/></span>
+ 
+ </div>
+ </>
+,document.getElementById('root')
+  );
+
+
+
